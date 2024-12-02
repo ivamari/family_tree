e68f5ce091dc3492a479a9fba3fd558212327202
ivameri <ivamerii@yandex.ru>
2024-12-02 20:02:25 +0200

    add AncestorSerializer to peoples app

--- peoples/serializers.py
+++ peoples/serializers.py
@@ -13,3 +13,21 @@ class PeopleSerializer(serializers.ModelSerializer):
             'mother_id',
             'father_id',
         )
+
+
+class AncestorSerializer(serializers.ModelSerializer):
+    mother = serializers.SerializerMethodField()
+    father = serializers.SerializerMethodField()
+
+    class Meta:
+        model = People
+        fields = ('id',
+                  'first_name',
+                  'last_name',
+                  'mother', 'father')
+
+    def get_mother(self, obj):
+        return AncestorSerializer(obj.mother_id).data if obj.mother else None
+
+    def get_father(self, obj):
+        return AncestorSerializer(obj.father_id).data if obj.father else None
